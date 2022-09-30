78b6fa3b2b1cce5613d4149f58abf0a0c4cadde7
adeelhunzai <adeehux@gmail.com>
2022-09-30 10:01:51 -0700

    used if statement to output persons list

--- src/App.js
+++ src/App.js
@@ -27,36 +27,28 @@ class App extends Component {
         { name: "mars", age: 22 },
         { name: event.target.value, age: 23 },
         { name: "duke", age: 44 },
-      ]
-      
+      ],
     });
   };
 
-   tooglePersonsHandler = () => {
-      const doesShow = this.state.showPersons;
-      this.setState({showPersons:!doesShow})
-    };
+  tooglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
   render() {
-    const btnStyle ={
-      width:'50%',
-      backgroundColor: 'Orange',
-      border: '2px solid #fefefe',
-      fontSize : '20pt',
-      padding : '18px',
-      marginBottom : '10px'
-    }
+    const btnStyle = {
+      width: "50%",
+      backgroundColor: "Orange",
+      border: "2px solid #fefefe",
+      fontSize: "20pt",
+      padding: "18px",
+      marginBottom: "10px",
+    };
 
-    return (
-      <div className="App">
-        <h1> Hi, I'm a react App.</h1>
-
-
-        <button 
-        style={btnStyle}
-        onClick={this.tooglePersonsHandler}>Toggle Handler Button</button>
-
-        { this.state.showPersons ?
-          <div>
+    let person = null;
+    if (this.state.showPersons) {
+      person = (
+        <div>
           <Person
             name={this.state.persons[0].name}
             age={this.state.persons[0].age}
@@ -65,7 +57,7 @@ class App extends Component {
           </Person>
           <Person
             name={this.state.persons[1].name}
-            click={() =>this.switchNameHandler('Maximisldjflisd')}
+            click={() => this.switchNameHandler("Maximisldjflisd")}
             age={this.state.persons[1].age}
             change={this.nameChangedHandler}
           />
@@ -73,8 +65,18 @@ class App extends Component {
             name={this.state.persons[2].name}
             age={this.state.persons[2].age}
           />
-        </div> :null
-        }
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <h1> Hi, I'm a react App.</h1>
+
+        <button style={btnStyle} onClick={this.tooglePersonsHandler}>
+          Toggle Handler Button
+        </button>
+        {person}
       </div>
     );
   }
