56c846f6adf827cbf6045ac5912a369004bc6e39
adeelhunzai <adeehux@gmail.com>
2022-09-30 11:50:05 -0700

    added key prop to component for generating lists uisng map

--- src/App.js
+++ src/App.js
@@ -16,15 +16,16 @@ class App extends Component {
   nameChangedHandler = (event) => {
     this.setState({
       persons: [
-        { name: "mars", age: 22 },
-        { name: event.target.value, age: 23 },
-        { name: "duke", age: 44 },
+        { personId: "1s1", name: "mars", age: 22 },
+        { personId: "1s2", name: event.target.value, age: 23 },
+        { personId: "1s3", name: "duke", age: 44 },
       ],
     });
   };
 
   deletePersonHandler = (personIndex) =>{
-    const persons = this.state.persons;
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
   this.setState({persons:persons})
   }
@@ -53,6 +54,7 @@ class App extends Component {
                   click={()=>this.deletePersonHandler(index)}
                   name={person.name}
                   age ={person.age}
+                  key={person.personId}
               />
             })
           }
